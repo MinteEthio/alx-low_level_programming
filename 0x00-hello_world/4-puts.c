de10d8fcf16f2196af2e9502a1ebdf4bff07c2b4
MinteEthio <mnte.tsegaye@gmail>
2022-12-12 05:48:10 -0800

    4. Hello, puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h> /* header file */
+
+int main() /*function */
+{
+  char Cstr[]="Programming is like building a multilingual puzzle\n"; /*string initialation*/
+
+  puts(Cstr); /*writing the string to stdout */
+  return 0;
+}
