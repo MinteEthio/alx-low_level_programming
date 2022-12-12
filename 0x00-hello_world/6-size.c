39d808d3f1ddeac998bd23bf3dc053aed72edce2
MinteEthio <mnte.tsegaye@gmail>
2022-12-12 13:48:33 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,7 +13,7 @@ int main (void)
 	float f;
 	printf("Size of a char: %lu bytes \n",(unsigned long)sizeof(c));
 	printf("Size of an int:%lu bytes \n",(unsigned long)sizeof(i));
-	printf("Size of a long int:%lu bytes \n",(unsigned long)sizeof(li));
+	printf("Size of a long long int:%lu bytes \n",(unsigned long)sizeof(li));
 	printf("Size of a float is:%lu bytes \n",(unsigned long)sizeof(f));
 return (0);
 }
