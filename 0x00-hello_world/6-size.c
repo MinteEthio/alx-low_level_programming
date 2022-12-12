661c179dfbb3b34b67a19c96d63b2743196ce63f
MinteEthio <mnte.tsegaye@gmail>
2022-12-12 13:04:00 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 int main (void)
+/**
+* main - Entry point
+*
+*
+* Return: Always 0
+*/
 {
 	prinf("Size of a char: %d bytes \n",sizeof(char));
 	prinf("Size of an int:%d bytes \n",sizeof(int));
