c244347f98b6a30938dc57d4980e0f5a1f79542e
MinteEthio <mnte.tsegaye@gmail>
2022-12-12 13:08:01 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,9 +7,13 @@ int main (void)
 * Return: Always 0
 */
 {
-	prinf("Size of a char: %d bytes \n",sizeof(char));
-	prinf("Size of an int:%d bytes \n",sizeof(int));
-	prinf("Size of a long int:%d bytes \n",sizeof(long int));
-	prinf("Size of a float is:%dbytes \n",sizeof(float));
+	char c;
+	int i;
+	long int li;
+	float f;
+	prinf("Size of a char: %d bytes \n",sizeof(c));
+	prinf("Size of an int:%d bytes \n",sizeof(i));
+	prinf("Size of a long int:%d bytes \n",sizeof(li));
+	prinf("Size of a float is:%dbytes \n",sizeof(f));
 return (0);
 }
