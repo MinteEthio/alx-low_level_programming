098c1c0fa3fb02f03f55e7a19bf645cf4b15d68f
MinteEthio <mnte.tsegaye@gmail>
2022-12-13 02:35:51 -0800

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,16 +5,16 @@
 *
 * Return: Always 0 (Success)
 */
-int main(void) 
+int main(void)
 {
 	char c;
 	int i;
 	long int li;
 	float f;
 
-	printf("Size of a char:%lu bytes \n",(unsigned long)sizeof(c));
-	printf("Size of an int:%lu bytes \n",(unsigned long)sizeof(i));
-	printf("Size of a long int:%lu bytes \n",(unsigned long)sizeof(li));
-	printf("Size of a float is:%lu bytes \n",(unsigned long)sizeof(f));
+	printf("Size of a char:%lu bytes \n", (unsigned long)sizeof(c));
+	printf("Size of an int:%lu bytes \n", (unsigned long)sizeof(i));
+	printf("Size of a long int:%lu bytes \n", (unsigned long)sizeof(li));
+	printf("Size of a float is:%lu bytes \n", (unsigned long)sizeof(f));
 return (0);
 }
