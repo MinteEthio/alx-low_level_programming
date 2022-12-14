2e8f87d2b9b4ebd305ecda6fcc8a638926aed2a4
MinteEthio <mnte.tsegaye@gmail>
2022-12-14 03:46:20 -0800

    3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+*main -Entry start here
+*
+*Return: Always 0 (Success)
+*/
+int main(void)
+{
+{char CHR;
+for (CHR = 'A'; CHR <= 'Z' ; chr++)
+{
+putchar (CHR);
+}
+{
+putchar ('\n');
+}
+
+char chr;
+for (chr = 'a'; chr <= 'z' ; chr++)
+{
+putchar (chr);
+}
+{
+putchar ('\n');
+}
+return (0);
+}
+
