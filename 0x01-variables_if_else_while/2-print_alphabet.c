479f3368f636455c6c6e220376dfea4eda6d8a7e
MinteEthio <mnte.tsegaye@gmail>
2022-12-13 21:05:52 -0800

    2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <conio.h>
+#include <stdio.h>
+/**
+*main -Entry start here
+*
+*Return: Always 0 (Success)
+*/
+int void(0)
+{
+int chr;
+clrscr();
+for (chr = 'a'; chr < = 'z' ; chr++)
+printf(chr \n);
+return (0);
+}
+
