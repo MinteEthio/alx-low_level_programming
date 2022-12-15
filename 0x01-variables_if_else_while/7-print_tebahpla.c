3a297a0828c0224a0373fd20151b789076f5c55d
MinteEthio <mnte.tsegaye@gmail>
2022-12-15 05:02:02 -0800

    7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+*main -Entry start here
+*
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char chr;
+
+for (chr = 'z'; chr >= 'a' ; chr++)
+{
+putchar (chr);
+}
+{
+putchar ('\n');
+}
+return (0);
+}            
