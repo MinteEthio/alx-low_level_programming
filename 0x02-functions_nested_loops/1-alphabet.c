b2fcb109e70b723ce622963725056dea1a9bcd47
MinteEthio <mnte.tsegaye@gmail>
2022-12-18 23:46:55 -0800

    0

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *main - check the code
+ *
+ *Return: Always 0.
+ */
+int main(void)
+{
+char chr;
+for (chr = 'a'; chr <= 'z'; chr++)
+{
+_putchar(chr);
+}
+_putchar('\n');
+return (0);
+}
