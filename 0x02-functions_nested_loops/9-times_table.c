b2b82412f7acec45dabda5d2dfd8d01f8908abb0
MinteEthio <mnte.tsegaye@gmail>
2022-12-19 12:20:44 -0800

    9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ *
+ * Description: prints the 9 times table string with 0
+ *
+ * Return 0
+ */
+
+void times_table(void)
+{
+	int a, b, c;
+
+	for (a = 0; b < 10; b++)
+	{
+		for (a = 0; a < 10; a++)
+		{
+			c = a * b;
+			if (a == 0)
+			{
+				_putchar(c + '0');
+			}
+			else if (c >= 10)
+			{
+				_putchar('  ');
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+			else
+			{
+				_putchar('  ');
+				_putchar(c / 10 + '0');
+				_putchar(c % 10 + '0');
+			}
+			else
+			{
+				_putchar('  ');
+				_putchar('  ');
+				_putchar(c + '0');
+			}
+			if (a != 9)
+			{
+				_putchar(',');
+			}
+		}
+		_putchar('\n');
+	}
+}
