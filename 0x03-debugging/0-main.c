752c9833007e9ab1919300b82d5b268b1fd6ce0e
MinteEthio <mnte.tsegaye@gmail>
2022-12-19 14:54:37 -0800

    0

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *main - teste function that prints if int. is positive || negative
+ *Return: 0
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
---
+++ 0x03-debugging/main.h
@@ -0,0 +1,8 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+void positive_or_egative(int i);
+
+#endif /* MAIN_H */
