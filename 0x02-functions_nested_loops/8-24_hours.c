abcecc662dbcc89bddacf4d142289ca4f39abb5c
MinteEthio <mnte.tsegaye@gmail>
2022-12-19 11:58:45 -0800

    8

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * jack_bauer - print every min of the day
+ *
+ * Description: function that prints every min
+ *
+ * Return: 0
+ */
+
+void jack_bauer(void)
+{
+	int h;
+
+	for (h = 0; h < 24; h++)
+
+	{
+		int m;
+
+		for (m = 0; m < 60; m++)
+
+		{
+			_putchar('0' + (h / 10));
+			_putchar('0' + (h % 10));
+			_putchar(':');
+			_putchar('0' + (m / 10));
+			_putchar('0' + (m % 10));
+			_putchar('\n');
+		}
+	}
+}
