c7e580f87a5474272b4b67e4f0de2fab7d5a8b15
MinteEthio <mnte.tsegaye@gmail>
2022-12-19 15:22:00 -0800

    2

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -4,5 +4,8 @@
 #include <stdio.h>
 
 void positive_or_egative(int i);
+int largest_number(int a, int b, int c);
+void print remaning_days(int month, int day, int year);
+int convert_day(int month, int day);
 
 #endif /* MAIN_H */
