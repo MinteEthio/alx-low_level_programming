d986ae2af58c0a4707c8d00a722cf8b96f7ef2aa
MinteEthio <mnte.tsegaye@gmail>
2022-12-19 11:01:50 -0800

    6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _abs -print absolute value
+ *@j: argument to be cheked
+ *
+ *Return: absolute value
+ */
+int _abs(int j)
+{
+if (j < 0)
+{
+j = -1 * j;
+}
+return (j);
+}
