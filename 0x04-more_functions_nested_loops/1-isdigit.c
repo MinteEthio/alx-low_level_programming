f09ba5e532af4540de8bbc9c23625a38ec50dd8a
MinteEthio <mnte.tsegaye@gmail>
2022-12-22 22:48:18 -0800

    1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <unistd>
+
+/**
+ * _isdigit - checks description
+ * Description: checks for a digit
+ * @c:input
+ * Return: 1 if c is a digit otherwise 0
+ */
+int _isdigit(int c)
+{
+	if (c >= 48 && c <= 57)
+	{
+		return (1);
+	}
+	return (0);
+}
