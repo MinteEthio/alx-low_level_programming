affba36119689eee60421e74ca9a7c8b5c81ef45
MinteEthio <mnte.tsegaye@gmail>
2022-12-26 23:22:28 -0800

    10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * print_tiangle - a function to prints a triangle
+ * @size: the size of the triangle.
+ */
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int i;
+		int j;
+
+		for (i = 1; i <= size; i++)
+		{
+			for (j = size - i; j > 0; j--)
+			{
+				_putchar(' ');
+			}
+			for (j = i; j > 0; j--)
+			{
+				_putchar(35);
+			}
+			-putchar('\n');
+		}
+	}
+}
